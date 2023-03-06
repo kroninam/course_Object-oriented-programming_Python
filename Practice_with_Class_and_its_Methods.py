4bd5fab03c50f36795f2057b0515a1f50e46e500
kr <kroninam@gmail.com>
2023-03-06 11:56:16 +0300

    morning_06_March_2023

--- Practice_with_Class_and_its_Methods.py
+++ Practice_with_Class_and_its_Methods.py
@@ -346,4 +346,130 @@ emp.work.display_company_info()
 
 # NEXT TASK
 
+# Создание приложения для учета задач
+#
+# Необходимо создать приложение для учета задач, которое будет использовать классы Task, TaskList и TaskManager.
+#
+# Класс Task будет хранить информацию о задаче (название, описание, статус выполнения), состоит из:
+#
+# метода __init__, принимающий  и сохраняет в атрибуты экземпляра name , description и status. По умолчанию status равен False.
+#
+# метод display , который печатает информацию в следующем виде:
+# {название задачи} (Сделана/Не сделана)
+# В зависимости от статуса задачи выводится Сделана или Не сделана
+#
+#
+# Класс TaskList будет содержать список задач и методы для добавления/удаления задач, состоит из:
+#
+# метода __init__, который должен создать пустой список задач в атрибуте tasks
+#
+# метода add_task, который принимает задачу и добавляет ее в конец списка задач
+#
+# метода remove_task, который принимает задачу и удаляет ее из списка задач
+#
+#
+# Класс TaskManager будет содержать методы для отображения списка задач и изменения статуса выполнения задач. В нем должно быть реализовано:
+#
+# метод __init__, который должен приманить экземпляр TaskList  и сохранять его в атрибуте  task_list
+#
+# метод  mark_done, который принимает задачу (экземпляр Task)  и устанавливает ей истинный статус выполнения
+#
+# метод  mark_undone, который принимает задачу (экземпляр Task)  и устанавливает ей ложный статус выполнения
+#
+# метод  show_tasks, который хранящейся задачи в списке вызывает метод display
+#
 
+class Task:
+
+    def __init__(self, name, description, status=False):
+        self.name = name
+        self.description = description
+        self.status = status
+
+    def display(self):
+        if self.status is True:
+            print(f'{self.name} Сделана')
+        else:
+            print(f'{self.name} Не сделана')
+
+class TaskList:
+
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, name):
+        self.tasks.append(name)
+
+    def remove_task(self, name):
+        self.tasks.remove(name)
+
+class TaskManager:
+
+    def __init__(self, task_list):
+        self.task_list = task_list
+
+    def mark_done(self, task):
+        task.status = True
+
+    def mark_undone(self, task):
+        task.status = False
+
+    def show_tasks(self):
+        #print(self.task_list.tasks)
+        for task in self.task_list.tasks:
+            task.display()
+
+# # Создаем менеджер задач и показываем список задач
+# manager = TaskManager(todo)
+# assert isinstance(manager.task_list, TaskList)
+# print('-----Список дел-----')
+# manager.show_tasks()
+
+# Напишите определение классов
+
+# Создаем список задач
+todo = TaskList()
+assert todo.tasks == []
+
+# Создаем несколько задач и добавляем их в список
+task1 = Task("Стирка", "Постирать трусы, носки, слюнявчики")
+assert task1.name == 'Стирка'
+assert task1.description == 'Постирать трусы, носки, слюнявчики'
+assert task1.status is False
+task1.display()
+
+todo.add_task(task1)
+assert len(todo.tasks) == 1
+
+task2 = Task("Продукты", "Купить лук чеснок огурцы хлеб и биток")
+assert task2.name == 'Продукты'
+assert task2.description == 'Купить лук чеснок огурцы хлеб и биток'
+assert task2.status is False
+
+todo.add_task(task2)
+assert len(todo.tasks) == 2
+
+# Создаем менеджер задач и показываем список задач
+manager = TaskManager(todo)
+assert isinstance(manager.task_list, TaskList)
+print('-----Список дел-----')
+manager.show_tasks()
+
+# Отмечаем первую задачу выполненной и показываем список задач
+manager.mark_done(task1)
+
+# Проверяем изменился ли статус 2мя способами
+assert task1.status is True
+assert manager.task_list.tasks[0].status is True
+
+print('-----Список дел-----')
+manager.show_tasks()
+
+# Удаляем вторую задачу и показываем список задач
+todo.remove_task(task2)
+
+assert len(todo.tasks) == 1
+assert len(manager.task_list.tasks) == 1
+
+print('-----Список дел-----')
+manager.show_tasks()
