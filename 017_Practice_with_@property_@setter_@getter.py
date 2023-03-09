fc1975da07ed3abdd7915ca4e030632ab068083a
kr <romanevichkirill@gmail.com>
2023-03-09 15:15:14 +0300

    Practice 9_March_2023

--- 017_Practice_with_@property_@setter_@getter.py
+++ 017_Practice_with_@property_@setter_@getter.py
@@ -131,6 +131,196 @@ class Registration:
 a = Registration('fg.@a')
 print(a.login)
 
+# Ниже код для проверки класса Registration
+
+try:
+    result = Registration("fga")
+except ValueError as error:
+    print("Логин fga должен содержать один символ '@'")
+
+try:
+    result = Registration(1234)
+except TypeError as error:
+    print("Пароль должен быть строкой")
+
+try:
+    result = Registration("f@ga@")
+except ValueError as error:
+    print("Логин f@ga@ должен содержать только один символ '@'")
+
+try:
+    result = Registration("fg@a")
+except ValueError as error:
+    print("В логине fg@a должен быть символ '.' после символа '@'")
+
+try:
+    result = Registration("fg.@a")
+except ValueError as error:
+    print("В логине fg.@a должна быть '.' после символа '@'")
+
+result = Registration("translate@gmail.com")
+assert result.login == "translate@gmail.com"
+assert result._Registration__login == "translate@gmail.com"
+
+try:
+    result.login = "asdsa12asd."
+except ValueError as error:
+    print("Логин asdsa12asd. должен содержать один символ '@'")
+
+try:
+    result.login = "asdsa12@asd"
+except ValueError as error:
+    print("asdsa12@asd должен быть символ '.' после символа '@'")
+
+result.login = "alligator13@how.do"
+assert result.login == "alligator13@how.do"
+assert result._Registration__login == "alligator13@how.do"
+
+# # Вторая часть задачи
+#
+# в метод  __init__ добавляется еще один аргумент: пароль. Как в примере с логином,
+# вы должны будете сохранить переданный пароль через password через сеттер  password
+# (см пункт 3 в этом задании). Примерный код метода __init__
+#
+# def __init__(self, логин, пароль):
+#     self.login = логин # передаем в сеттер login значение логин
+#     self.password = пароль # передаем в сеттер password значение пароль
+
+
+# Должны сработать свойства сеттер login из предыдущего задания  и сеттер password из
+# пункта 3 для проверки валидности переданных значений
+#
+# Свойство геттер password, которое возвращает значение self.__password;
+#
+# Свойство сеттер password, принимает значение нового пароля. Его необходимо перед сохранением проверить на следующее:
+#
+# новое значение пароля должно быть строкой(не список, словарь и т.д. ) в противном случае вызываем исключение
+# TypeError("Пароль должен быть строкой")
+
+# Длина нового пароля должна быть от 5 до 11 символов, в противном случае вызывать исключение
+# ValueError('Пароль должен быть длиннее 4 и меньше 12 символов')
+
+# Новый пароль должен содержать хотя бы одну цифру. Для этого создаем staticmethod is_include_digit ,
+# который проходит по всем элементам строки и проверяет наличие цифр. В случае отсутствия цифрового
+# символа вызываем исключение: ValueError('Пароль должен содержать хотя бы одну цифру')
+
+# Строка password должна содержать элементы верхнего и нижнего регистра. Создаем staticmethod is_include_all_register,
+# который с помощью цикла проверяет элемента строчки на регистр. В случае ошибки
+# вызываем: ValueError('Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
+
+# Строка password помимо цифр должна содержать только латинские символы. Для этого
+# создайте staticmethod is_include_only_latin , который проверяет каждый элемент нового значения
+# на принадлежность к латинскому алфавиту(проверка должна быть как в верхнем, так и нижнем регистре).
+# В случае, если встретится нелатинский символ, вызвать ошибку ValueError('Пароль должен содержать
+# только латинский алфавит').
+# Подсказка: из модуля string можно импортировать переменную ascii_letters, она хранит в себе все латинские символы
+# в верхнем и нижнем регистре
+
+# Пароль не должен совпадать ни с одним из легких паролей, хранящихся в файле easy_passwords.txt.
+# Сохраните данный файл к себе в папку с вашей программой и не меняйте название. С помощью staticmethod
+# создаем метод check_password_dictionary и проверяем наличие нашего пароля в данном файле.
+# Если значение совпадет со значением из файла, то в сеттер вызываем
+# исключение: ValueError('Ваш пароль содержится в списке самых легких')
+
+from string import ascii_lowercase, ascii_uppercase, digits, ascii_letters
+
+class Registration:
+
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+    @property
+    def login(self):
+        return self.__login
+
+    @login.setter
+    def login(self, new_login):
+        if type(new_login) != str:
+            raise TypeError
+        if '@' not in new_login:
+            raise ValueError
+        if '.' not in new_login and new_login.index('@') < new_login.index('.'):
+            raise ValueError
+        else:
+            self.__login = new_login
+
+    @property
+    def password(self):
+        return self.__password
+
+    @staticmethod
+    def is_include_digit(password):
+        for i in password:
+            if i in digits:
+                return True
+            else:
+                continue
+        return False
+
+    @staticmethod
+    def is_include_all_register(password):
+        x = 0
+        y = 0
+        for i in password:
+            if i in ascii_lowercase:
+                x +=1
+            if i in ascii_uppercase:
+                y +=1
+        if x==0 and y==0:
+            return False
+
+    @staticmethod
+    def is_include_only_latin(password):
+        for i in password:
+            if i not in digits:
+                if i in ascii_letters:
+                    pass
+                else:
+                    return False
+
+    @staticmethod
+    def check_password_dictionary(password):
+        with open('easy_passwords.txt') as p_list:
+            if password in p_list.read().strip():
+                return False
+            else:
+                return True
+
+    @password.setter
+    def password(self, value):
+        if type(value) != str:
+            raise TypeError ('Пароль должен быть строкой')
+
+        if self.is_include_digit(value) is False:
+            raise ValueError('Пароль должен содержать хотя бы одну цифру')
+
+        if len(value) < 5 or len(value) > 12:
+            raise ValueError ('Пароль должен быть длиннее 4 и меньше 12 символов')
+
+        if self.is_include_all_register(value) is False:
+            raise ValueError('Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
+        if self.is_include_only_latin(value) is False:
+            raise ValueError('Пароль должен содержать только латинский алфавит')
+        if self.check_password_dictionary(value) is False:
+            raise ValueError('Ваш пароль содержится в списке самых легких')
+
+        else:
+            self.__password = value
+
+
+# Пример работы класса Registration
+
+r1 = Registration('qwerty@rambler.ru', 'QwrRt24') # здесь хороший логин
+print(r1.login, r1.password)  # qwerty@rambler.ru QwrRt124
+
+# теперь пытаемся запись плохие пароли
+r1.password = '1111234567'  # ValueError('Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
+print(r1.login, r1.password)  # qwerty@rambler.ru QwrRt124
+# r1.password = 'LoW'  # raise ValueError('Пароль должен быть длиннее 4 и меньше 12 символов')
+# r1.password = 43  # raise TypeError("Пароль должен быть строкой")
+
+
 
 
 
---
+++ 019_More&More_Practice_with_@property_@setter_@getter.py
@@ -0,0 +1,6 @@
+## # # # Артем Егоров
+# # # # Автор и бессменный ведущий образовательного канала по разработке на Python https:
+# # # # //www.youtube.com/c/egoroffchannel
+
+# Practice with @property, @setter, @getter
+
