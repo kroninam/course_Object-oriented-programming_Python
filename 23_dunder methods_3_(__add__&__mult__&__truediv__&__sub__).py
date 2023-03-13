4af3f65e2cf9d545fe6dc5accce1fda919e1dbd1
kr <romanevichkirill@gmail.com>
2023-03-13 12:49:11 +0300

    new_solved_tasks_today_13_March_2023

--- 23_dunder methods_3_(__add__&__mult__&__truediv__&__sub__).py
+++ 23_dunder methods_3_(__add__&__mult__&__truediv__&__sub__).py
@@ -90,5 +90,201 @@ print(r3)
 
 # NEXT TASK #####
 
+# Создайте класс  Order, который имеет следующие методы:
+#
+# метод __init__, который устанавливает значения атрибутов cart и customer: список покупок и имя покупателя
+#
+# магический метод __add__, который описывает добавления товара в список покупок.
+# Результатом такого сложения должен быть новый заказ, в котором все покупки берутся из
+# старого заказа и в конец добавляется новый товар. Покупатель в заказе остается прежним
+#
+# магический метод __radd__, который описывает добавления товара в список покупок
+# при правостороннем сложении. Результатом такого сложения должен быть новый заказ,
+# в котором все покупки берутся из старого заказа и в начало списка покупок добавляется
+# новый товар. Покупатель в заказе остается прежним
+#
+# магический метод __sub__, который описывает исключение товара из списка покупок.
+# Результатом вычитания должен быть новый заказ
+#
+# магический метод __rsub__, который описывает исключение товара из списка покупок
+# при правостороннем вычитании. Результатом должен быть таким же как и при __sub__
+from collections import Counter
+class Order:
+
+    def __init__(self, cart, customer):
+
+        self.cart = cart
+        self.customer = customer
+
+    def __add__(self, other):
+        new_list = [other]
+        return Order(self.cart + new_list, self.customer)
+
+
+    def __radd__(self, other):
+        new_list = [other]
+        return Order(new_list + self.cart, self.customer)
+
+    def __sub__(self, other):
+        new_value = [other]
+        c = []
+        if other in self.cart:
+
+            for i in self.cart:
+                if not (i in new_value):
+                    c.append(i)
+            return Order(c, self.customer)
+        else:
+            return Order(self.cart, self.customer)
+
+    def __rsub__(self, other):
+        new_value = [other]
+        c = []
+        if other in self.cart:
+
+            for i in self.cart:
+                if not (i in new_value):
+                    c.append(i)
+            return Order(c, self.customer)
+        else:
+            return Order(self.cart, self.customer)
+
+# Ниже код для проверки методов класса Order
+
+order = Order(['banana', 'apple'], 'Гена Букин')
+
+order_2 = order + 'orange'
+assert order.cart == ['banana', 'apple']
+assert order.customer == 'Гена Букин'
+assert order_2.cart == ['banana', 'apple', 'orange']
+
+order = 'mango' + order
+assert order.cart == ['mango', 'banana', 'apple']
+order = 'ice cream' + order
+assert order.cart == ['ice cream', 'mango', 'banana', 'apple']
+
+order = order - 'banana'
+print(order.cart)
+
+
+assert order.cart == ['ice cream', 'mango', 'apple']
+
+order3 = order - 'banana'
+assert order3.cart == ['ice cream', 'mango', 'apple']
+
+order = order - 'mango'
+assert order.cart == ['ice cream', 'apple']
+order = 'lime' - order
+assert order.cart == ['ice cream', 'apple']
+print('Good')
+
+## NEXT TASK
+
+# Ваша задача создать класс Vector, который хранит в себе вектор целых чисел.  У класса Vector есть:
+#
+# конструктор __init__, принимающий произвольное количество аргументов. Среди всех переданных
+# аргументов необходимо оставить только целые числа и сохранить их в атрибут values в виде списка.
+# Причем значения должны хранится в порядке неубывания. В случае, если целых чисел не передано,
+# нужно в атрибут values сохранять пустой список;
+#
+# переопределить метод __str__ так, чтобы экземпляр класса Vector выводился следующим образом:
+# "Вектор(<value1>, <value2>, <value3>, ...)", если вектор не пустой. При этом значения должны быть
+# упорядочены по возрастанию; "Пустой вектор", если наш вектор не хранит в себе значения.
+
+class Vector:
+
+    def __init__(self, *args):
+        self.values = []
+        for i in args:
+            if isinstance(i, int):
+                self.values.append(i)
+                self.values.sort()
+
+    def __str__(self):
+        if len(self.values) > 0:
+            ans = ''
+            for i in sorted(self.values):
+                ans += str(i) + ', '
+
+            return f'Вектор({ans.rstrip(", ")})'
+        else:
+            return f'Пустой вектор'
+
+# *переопределить метод __add__ так, чтобы экземпляр класса Vector мог складываться
+# с целым числом, в результате должен получиться новый Vector, у которого каждый элемент
+# атрибута values увеличен на число
+
+# *с другим вектором такой же длины. В результате должен получиться новый Vector,
+# состоящий из суммы элементов, расположенных на одинаковых местах.
+# Если длины векторов различаются, выведите сообщение
+# "Сложение векторов разной длины недопустимо";
+
+
+# *В случае, если вектор складывается с другим типом(не числом и не вектором),
+# нужно вывести сообщение "Вектор нельзя сложить с <значением>".
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            new_args_1 = []
+            for i in self.values:
+                new_args_1.append(i + other)
+            return Vector(*new_args_1)
+
+        elif isinstance(other, Vector):
+            new_args_2 = []
+            if len(self.values) == len(other.values):
+                for i in range(len(self.values)):
+                    new_args_2.append(self.values[i] + other.values[i])
+                return Vector(*new_args_2)
+
+        else:
+            print(f'Вектор нельзя сложить с {other}')
+
+# переопределить метод __mul__ так, чтобы экземпляр класса Vector мог умножаться
+#
+# *на целое число. В результате должен получиться новый Vector, у которого каждый
+# элемент атрибута values умножен на переданное число;
+
+#
+# *на другой вектор такой же длины. В результате должен получиться новый Vector,
+# состоящий из произведения элементов, расположенных на одинаковых местах.
+# Если длины векторов различаются, выведите сообщение "Умножение векторов разной длины недопустимо";
+#
+#
+# *В случае, если вектор умножается с другим типом(не числом и не вектором),
+# нужны вывести сообщение "Вектор нельзя умножать с <значением>";
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            new_args_3 = []
+            for i in self.values:
+                new_args_3.append(i * other)
+            return Vector(*new_args_3)
+
+        elif isinstance(other, Vector):
+            new_args_2 = []
+            if len(self.values) == len(other.values):
+                for i in range(len(self.values)):
+                    new_args_2.append(self.values[i] * other.values[i])
+                return Vector(*new_args_2)
+            else:
+                print(f'Умножение векторов разной длины недопустимо')
+
+        else:
+            print(f'Вектор нельзя умножать с {other}')
+
+
+v1 = Vector(1,2,3)
+print(v1) # печатает "Вектор(1, 2, 3)"
+
+v2 = Vector(3,4,5)
+print(v2) # печатает "Вектор(3, 4, 5)"
+v3 = v1 + v2
+print(v3) # печатает "Вектор(4, 6, 8)"
+v4 = v3 + 5
+print(v4) # печатает "Вектор(9, 11, 13)"
+
+v5 = v4+555
+print(v5) # печатает "Вектор(9, 11, 13)"
 
 
